901d5971546c3e23342371abe40373a921cec041
Kayla Clements <clemekay@oregonstate.edu>
2025-03-06 13:12:43 -0800

    Calculate and generate repetition boxplots for stoch ishigami si and ti

--- GSA_code/ishigami_input.py
+++ GSA_code/ishigami_input.py
@@ -32,7 +32,7 @@ def ishigami_single_repetition(n_xi, n_eta):
     return var, vi, eni, si, ti, qoi
 
 
-def draw_plot(data, offset, edge_color, ax, fill_color="white"):
+def draw_box(data, offset, edge_color, ax, fill_color="white"):
     pos = np.arange(data.shape[1])+offset+1
     bplot = ax.boxplot(data, positions= pos, widths=0.3, patch_artist=True)
     for element in ['boxes', 'medians']:
@@ -48,7 +48,31 @@ def draw_plot(data, offset, edge_color, ax, fill_color="white"):
     return bplot
 
 
-def run_ishigami_example(n_eta, n_xi, n_reps, figname=False):
+def boxplot_repetition_data(analytic, data, n_eta, n_xi, n_reps, ylabel, ylim, figname=False):
+    fig, ax = plt.subplots()
+    # Analytic
+    for i in range(3):
+        x = np.array([-1, 1])*0.5 + i + 1
+        y = np.ones(2) * analytic[i]
+        anplot = ax.plot(x, y, '*-', color='black', markersize=10)
+    # Numerical
+    A = data[:,:,0]
+    B = data[:,:,1]
+    stanplot = draw_box(A, -0.2, "red", ax)
+    vardplot = draw_box(B, +0.2, "skyblue", ax)
+    plt.xticks([1,2,3], [1,2,3])
+    ax.legend([anplot[0], stanplot['boxes'][0], vardplot['boxes'][0]], ['Analytic', 'Standard', 'Var dec.'], loc='upper right')
+    plt.xlabel("Input number")
+    plt.ylabel(ylabel)
+    ax.set_title(r'$N_{\eta}=$' + str(n_eta) + r'$, \,\, N_{\xi}=$' + str(n_xi) + r'$, \,\, N_{reps}=$' + str(n_reps))
+    ax.set_ylim(ylim)
+    if figname:
+        # don't know why but the working directory is the parent directory instead of GSA_code
+        # this will make the figure save, but when there's time I'd like to know why that's happening
+        plt.savefig(figname)
+
+
+def run_ishigami_example(n_eta, n_xi, n_reps):
     qoi = np.zeros(n_reps)
     var = np.zeros((n_reps, 2))
     vi = np.zeros((n_reps, 3, 2))
@@ -57,30 +81,36 @@ def run_ishigami_example(n_eta, n_xi, n_reps, figname=False):
     for rep in range(n_reps):
         var[rep], vi[rep], eni[rep], si[rep], ti[rep], qoi[rep] = ishigami_single_repetition(n_xi, n_eta)
 
-    
-    fig, ax = plt.subplots()
-    # Analytic
-    for i in range(3):
-        x = np.array([-1, 1])*0.5 + i + 1
-        y = np.ones(2) * a_si[i]
-        anplot = ax.plot(x, y, '*-', color='black', markersize=10)
-    # Numerical
-    A = si[:,:,0]
-    B = si[:,:,1]
-    stanplot = draw_plot(A, -0.2, "red", ax)
-    vardplot = draw_plot(B, +0.2, "skyblue", ax)
-    plt.xticks([1,2,3], [1,2,3])
-    ax.legend([anplot[0], stanplot['boxes'][0], vardplot['boxes'][0]], ['Analytic', 'Standard', 'Var dec.'], loc='upper right')
-    plt.xlabel("Input number")
-    plt.ylabel("First-order Sobol' index")
-    ax.set_title(r'$N_{\eta}=$' + str(n_eta) + r'$, \,\, N_{\xi}=$' + str(n_xi) + r'$, \,\, N_{reps}=$' + str(n_reps))
-    ax.set_ylim([-0.5, 1])
-    if figname:
-        # don't know why but the working directory is the parent directory instead of GSA_code
-        # this will make the figure save, but when there's time I'd like to know why that's happening
-        plt.savefig('GSA_code/ishigami_figures/eta{0}_xi{1}_reps{2}.png'.format(n_eta, n_xi, n_reps))
+    return var, vi, eni, si, ti, qoi
 
 
+def boxplot_ti(n_eta, n_xi, n_reps, ti, savefig=False):
+    if savefig:
+        name ='GSA_code/ishigami_figures/ti_eta{0}_xi{1}_reps{2}.png'.format(n_eta, n_xi, n_reps)
+    else:
+        name=False
+    boxplot_repetition_data(analytic=a_ti, data=ti, n_eta=n_eta, n_xi=n_xi, n_reps=n_reps, 
+                            ylabel="Total-order Sobol' index", ylim=[0,1.1], figname=name)
+
+
+def boxplot_si(n_eta, n_xi, n_reps, si, savefig=False):
+    if savefig:
+        name ='GSA_code/ishigami_figures/si_eta{0}_xi{1}_reps{2}.png'.format(n_eta, n_xi, n_reps)
+    else:
+        name=False
+    boxplot_repetition_data(analytic=a_si, data=si, n_eta=n_eta, n_xi=n_xi, n_reps=n_reps, 
+                            ylabel="First-order Sobol' index", ylim=[-0.5,1], figname=name)
+
+
+def run_and_plot_ti(n_eta, n_xi, n_reps, savefig=False):
+    var, vi, eni, si, ti, qoi = run_ishigami_example(n_eta, n_xi, n_reps)
+    boxplot_ti(n_eta, n_xi, n_reps, ti, savefig)
+
+
+def run_and_plot_indices(n_eta, n_xi, n_reps, savefig=False):
+    var, vi, eni, si, ti, qoi = run_ishigami_example(n_eta, n_xi, n_reps)
+    boxplot_si(n_eta, n_xi, n_reps, si, savefig)
+    boxplot_ti(n_eta, n_xi, n_reps, ti, savefig)
 
 # Instantiate gsa input_deck in gsa.functions
 k=3
@@ -92,18 +122,15 @@ a_var, a_vi, a_eni, a_si, a_ti, a_qoi = gsa.analytic_ishigami_indices()
 
 
 # Run repetitions of ishigami function and generate boxplot of results
-# run_ishigami_example(n_eta=5, n_xi=100, n_reps=1000, figname=True)            # baseline
-# run_ishigami_example(n_eta=10, n_xi=100, n_reps=1000, figname=True)           # n_eta * 2
-# run_ishigami_example(n_eta=5, n_xi=200, n_reps=1000, figname=True)            # n_xi * 2
-# run_ishigami_example(n_eta=50, n_xi=100, n_reps=1000, figname=True)           # n_eta * 10
-# run_ishigami_example(n_eta=5, n_xi=1000, n_reps=1000, figname=True)            # n_xi * 10
-# 
-# run_ishigami_example(n_eta=5, n_xi=100, n_reps=100, figname=True)            # baseline
-# run_ishigami_example(n_eta=10, n_xi=100, n_reps=100, figname=True)           # n_eta * 2
-# run_ishigami_example(n_eta=5, n_xi=200, n_reps=100, figname=True)            # n_xi * 2
-# run_ishigami_example(n_eta=50, n_xi=100, n_reps=100, figname=True)           # n_eta * 10
-# run_ishigami_example(n_eta=5, n_xi=1000, n_reps=100, figname=True)            # n_xi * 10
-run_ishigami_example(n_eta=50, n_xi=1000, n_reps=100, figname=True)
+eta =  [5, 50]
+xi =  [100, 1000]
+reps = [100, 1000]
+
+for bing in eta:
+    for bong in xi:
+        for borp in reps:
+            run_and_plot_indices(n_eta=bing, n_xi=bong, n_reps=borp, savefig=True)
+
 
 
 plt.show()
